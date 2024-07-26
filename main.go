192a7da3cd70a7772ee5695f9dc63ee7a3b77eab
Szulock <semen4ik.zubarev@gmail.com>
2024-07-26 16:34:57 +0300

    Реализуйте обработчик, который возвращает HTTP-заголовки запроса в JSON-формате.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", HelloHandler)
 	http.HandleFunc("/json", NewCoderjSON)
+	http.HandleFunc("/headerget", GetHeaders)
 	http.ListenAndServe(":8080", nil) // Запуск сервера
 }
 
@@ -20,4 +21,16 @@ func NewCoderjSON(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	mymessage := map[string]string{"Name": "Semen"}
 	json.NewEncoder(w).Encode(mymessage)
+}
+
+func GetHeaders(w http.ResponseWriter, r *http.Request) {
+	headers := r.Header
+	headersMap := make(map[string][]string)
+	for key, value := range headers {
+		headersMap[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	// Кодируем заголовки в JSON и отправляем в ответ
+	json.NewEncoder(w).Encode(headersMap)
 }
